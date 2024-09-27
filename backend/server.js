e8fe9902bb07d4649d5352854d9d92b42020a8bf
jyoti pal <paljyoti0129@gmail.com>
2024-09-27 18:24:46 +0530

    created crud review

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const connectDB = require("./config/db");
 const movieRouter = require("./routes/movieRoutes")
+const reviewRouter = require("./routes/reviewRoutes")
 connectDB();
 
 const app = express();
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/movie", movieRouter);
+app.use("/api/review", reviewRouter);
 const port = 8080;
 app.listen(port, () => console.log(`listening in port  number ${port}`))
